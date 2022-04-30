97a8991dff3ba389995b6620df4e260c47bfde18
Abrar Ali <abraraliiofficial@gmail.com>
2022-04-30 23:52:10 +0500

    exception handling

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -10,18 +10,43 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   getlocation();
-  // }
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
 
-  // Future<void> getlocation() async {
-    // ignore: unused_local_variable
-  //   Future<Position> position =
-  //     (await  Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.low)) as Future<Position>;
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
 
-  // }
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition();
+  }
+
   var locationMessage = "";
   void getCurrentlocation() async {
     // ignore: unused_local_variable
@@ -42,6 +67,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
         child: RaisedButton(
           onPressed: () {
             getCurrentlocation();
+            _determinePosition();
           },
           child: const Text('Get Location'),
         ),
@@ -49,3 +75,10 @@ class _LoadingScreenState extends State<LoadingScreen> {
     );
   }
 }
+
+// exception handler 
+
+// try {
+// if program does not run in try tag than tha alternative code in catch tag run 
+// } catch (e) {
+// }
